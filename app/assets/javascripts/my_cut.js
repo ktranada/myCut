dfb3376a680c369923c577c1ca7b7cd979f78413
Kevin Tran <trank.kevin@gmail.com>
2015-06-09 20:38:18 -0700

    initialize repo

---
+++ app/assets/javascripts/my_cut.js
@@ -0,0 +1,13 @@
+window.MyCut = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  MyCut.initialize();
+});
