f144e203a42b55f3b95f1101830f0243164e8ff1
Kevin Tran <trank.kevin@gmail.com>
2015-06-13 13:39:37 -0700

    navbar logo + styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612065152) do
+ActiveRecord::Schema.define(version: 20150613193140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 20150612065152) do
   end
 
   add_index "pictures", ["imageable_id", "imageable_type"], name: "index_pictures_on_imageable_id_and_imageable_type", using: :btree
+
+  create_table "ratings", force: :cascade do |t|
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.text     "body",                        null: false
