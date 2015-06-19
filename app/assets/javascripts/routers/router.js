b7ea25e24e677c78bbb4577f473ac9f770fea918
Kevin Tran <trank.kevin@gmail.com>
2015-06-18 19:29:37 -0700

    created BB router route for user search results

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -1,7 +1,7 @@
   MyCut.Routers.Router = Backbone.Router.extend({
   routes: {
     "":                                  "feedLandingPage",
-    "search?find_des=:dest&cur_loc:loc": "shopSearch"
+    "search?find_des=:des&cur_loc:loc": "shopSearch"
     "shops/new":                         "createShop",
     "shops/:id":                         "showShop",
     "shops/:id/review/new":              "newReview",
@@ -47,11 +47,26 @@
     this._swapView(newReviewForm);
   },
 
+  shopSearch:function(:des, :loc){
+    var searchResults = new MyCut.collections.Shops({
+      searchData: {
+        description: des,
+        location: loc
+      }
+    })
+
+    var landingPage = new MyCut.Views.LandingPage({
+      collection: searchData
+    })
+    this._swapView(landingPage);
+  },
+
   showShop: function(id) {
     var shopModel = MyCut.shops.getOrFetch(id);
     var shopView = new MyCut.Views.ShowShop({ model: shopModel});
     this._swapView(shopView);
   },
+
 
   _swapView: function(newView) {
     this._currentView && this._currentView.remove();
